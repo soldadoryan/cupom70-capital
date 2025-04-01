466931f000dd839d12e72c70590bd6739278e32b
Ryan Drumond <cadastro@ryandrumond.com>
2025-04-01 03:00:18 -0300

    feat: change text and price

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -16,13 +16,10 @@ export default function MoradaValley() {
           <main className={s.main}>
             <h2 className={s.title}>Seguir para o pagamento</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Após a aprovação do pagamento, os produtos serão entregues
+              automaticamente na cidade em que a compra foi efetuada. Estamos
+              falando de uma loja de FiveM, e o prazo máximo para entrega é de
+              até <b>4 horas após a aprovação da compra.</b>
             </p>
           </main>
           <div className={s.wrapResume}>
